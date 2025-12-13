6f9aee961ba1e2dfd56adef0af83890ec3e10137
Cosmo <rluke42@hotmail.co.uk>
2025-12-13 21:38:00 +0000

    state file update

--- Standard-build-Infrastructure/app-vms-public/main.tf
+++ Standard-build-Infrastructure/app-vms-public/main.tf
@@ -2,7 +2,7 @@ terraform {
     backend "azurerm" {
     resource_group_name = "terraform-deploy"  # Replace with your resource group name
     storage_account_name = "terraformsflg"  # Replace with your storage account name
-    container_name = "ag-vms-uk"  # Replace with your desired container name
+    container_name = "app-vms-public"  # Replace with your desired container name
     key = "terraform.tfstate"  # Optional: Specify the filename within the container (defaults to 'terraform.tfstate')
   }
   required_providers {
