f6a9d03a52a779503edfae861b7eb9ff99e45b18
Cosmo <rluke42@hotmail.co.uk>
2025-12-13 21:33:28 +0000

    NIC update

--- Standard-build-Infrastructure/app-vms-public/modules/network/outputs.tf
+++ Standard-build-Infrastructure/app-vms-public/modules/network/outputs.tf
@@ -1,11 +1,6 @@
-output "network_interface_id" {
-  value = azurerm_network_interface.network.id
-}
+
 output "subnet_id" {
   value = azurerm_subnet.network.id
-}
-output "nic_name" {
-  value = var.networkinterface_name
 }
 output "public_ip_id" {
   value = azurerm_public_ip.vm_pip.id
